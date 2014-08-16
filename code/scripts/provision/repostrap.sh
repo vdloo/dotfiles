c87781a99c676d9dcedbf17f768798a00eee3ec7
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-16 20:25:22 +0000

    wget vim zenburn color scheme in repostrap

--- code/scripts/provision/repostrap.sh
+++ code/scripts/provision/repostrap.sh
@@ -3,12 +3,18 @@
 	&& git clone https://github.com/vdloo/dotfiles \
 		&& ( 	rm ~/.bashrc ; \
 			find dotfiles/ -mindepth 1 -maxdepth 1 ! -name '.git' -exec ln -s {} ~ ';'; \
-		) || echo "failed cloning dotfiles from public repo"
-cd ~
+		) || (cd dotfiles; git pull)
 type -p vim \
-	&& ( mkdir -p "./vim/bundle" ; cd "./vim/bundle" && (
-		[ ! -d Vundle.vim ] \
-			&& git clone https://github.com/gmarik/Vundle.vim.git \
-			|| (cd Vundle.vim; git pull Vundle.vim)
+	&& (	cd ~
+		mkdir -p ".vim/bundle" ; cd ".vim/bundle" && (
+			[ ! -d Vundle.vim ] \
+				&& git clone https://github.com/gmarik/Vundle.vim.git \
+				|| (cd Vundle.vim; git pull)
+		)
+		cd ~
+		mkdir -p ".vim/colors" ; cd ".vim/colors" && (
+			[ ! -f zenburn.vim ] \
+				&& wget -O zenburn.vim http://www.vim.org/scripts/download_script.php?src_id=15530
+		)
 		vim +PluginInstall +qall
 	)
