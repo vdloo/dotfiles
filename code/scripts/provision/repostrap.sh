366e48b98c5abf091b4b842ddd5db8510e754cfe
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-16 20:06:22 +0000

    install vim plugins if type -p vim

--- code/scripts/provision/repostrap.sh
+++ code/scripts/provision/repostrap.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 [ ! -d "dotfiles" ] \
 	&& git clone https://github.com/vdloo/dotfiles \
 		&& ( 	rm ~/.bashrc ; \
 			find dotfiles/ -mindepth 1 -maxdepth 1 ! -name '.git' -exec ln -s {} ~ ';'; \
 		) || echo "failed cloning dotfiles from public repo"
 cd ~
-mkdir -p "./vim/bundle" ; cd "./vim/bundle" && (
-	[ ! -d Vundle.vim ] \
-		&& git clone https://github.com/gmarik/Vundle.vim.git \
-		|| (cd Vundle.vim; git pull Vundle.vim)
-)
+type -p vim \
+	&& ( mkdir -p "./vim/bundle" ; cd "./vim/bundle" && (
+		[ ! -d Vundle.vim ] \
+			&& git clone https://github.com/gmarik/Vundle.vim.git \
+			|| (cd Vundle.vim; git pull Vundle.vim)
+		vim +PluginInstall +qall
+	)
