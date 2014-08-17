40264c48a1de657f01c8c757a47ba8c85f1a307b
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-17 10:35:46 +0000

    moved conditional to exclude getopt

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,81 +7,81 @@
 
 # example ./bootstrap.sh -p 1234 -u username -h example.com -n vagrant
 
+PROVISIONURL="https://raw.githubusercontent.com/vdloo/dotfiles/master/code/scripts/provision"
+unset USER
+unset REMOTEHOST
+unset PORT
+unset NONROOT
+PORT=22
+USER=$(logname)
+REMOTEHOST=0
+RET=0
 LATEST=-1
+
+while getopts "p:u:s:n:h" opt; do
+	case "$opt" in
+		p)
+			PORT="$OPTARG" ;;
+		u)
+			USER="$OPTARG" ;;
+		s)
+			REMOTEHOST="$OPTARG" ;;
+		n)
+			NONROOT="$OPTARG" ;;
+		h)
+			RET=1
+			;;
+	esac
+done
+
+if [ "$RET" == 1 ]; then
+	echo "Usage: ./bootstrap.sh [-s example.com] [-u user] [-p 443]"
+else
+
 [ -d ~/.dotfiles ] \
 	&& (	cd ~/.dotfiles
 		LATEST=$(git pull | grep "Already up-to-date." && echo 1 || echo 0)
 	)
 
 [ "$LATEST" != 0 ] \
-	&& (	PROVISIONURL="https://raw.githubusercontent.com/vdloo/dotfiles/master/code/scripts/provision"
-		unset USER
-		unset REMOTEHOST
-		unset PORT
-		unset NONROOT
-		PORT=22
-		USER=$(logname)
-		REMOTEHOST=0
-		RET=0
-		
-
-		while getopts "p:u:s:n:h" opt; do
-			case "$opt" in
-				p)
-					PORT="$OPTARG" ;;
-				u)
-					USER="$OPTARG" ;;
-				s)
-					REMOTEHOST="$OPTARG" ;;
-				n)
-					NONROOT="$OPTARG" ;;
-				h)
-					RET=1
-					;;
-			esac
-		done
-
-		if [ "$RET" == 1 ]; then
-			echo "Usage: ./bootstrap.sh [-s example.com] [-u user] [-p 443]"
-		else
-			if [ "$(id -u)" != "0" ]; then
-				echo "Run this script as root"
-			else 
-				# if executed during vagrant provisioning, copy scripts to home
-				if [ -d "/vagrant" ]; then
-					cp /vagrant/* /home/vagrant/
-				fi;
-
-				# update box and install specified programs
-				[ -f "setup.sh" ] && ./setup.sh
-
-				# download scripts from GitHub
-				RETRURL="${PROVISIONURL}/retrieve.sh"
-				[ ! -f "retrieve.sh" ] \
-					&& wget -A.sh "$RETRURL" \
-					&& chmod u+x retrieve.sh \
-					&& chown $NONROOT retrieve.sh
-				REPOURL="${PROVISIONURL}/repostrap.sh"
-				[ ! -f "repostrap.sh" ] \
-					&& wget -A.sh "$REPOURL" \
-					&& chmod u+x repostrap.sh \
-					&& chown $NONROOT repostrap.sh
-
-				# if remote host specified also provision from private repos through ssh
-				RLEN=$(echo "$REMOTEHOST" | wc -c)
-				if [ "$RLEN" -lt 4 ]; then
-					su $NONROOT -c "./retrieve.sh"
-					echo "run bootstrap.sh again with the -s flag to continue provisioning from private repos"
-				else
-					su $NONROOT -c "./retrieve.sh -u $USER -p $PORT -s $REMOTEHOST"
-				fi;
-
-				# cleanup provision scripts
-				[ -f "Vagrantfile" ]  && rm Vagrantfile
-				[ -f "retrieve.sh" ]  && rm retrieve.sh
-				[ -f "repostrap.sh" ] && rm repostrap.sh
+	&& (	if [ "$(id -u)" != "0" ]; then
+			echo "Run this script as root"
+		else 
+			# if executed during vagrant provisioning, copy scripts to home
+			if [ -d "/vagrant" ]; then
+				cp /vagrant/* /home/vagrant/
 			fi;
+
+			# update box and install specified programs
+			[ -f "setup.sh" ] && ./setup.sh
+
+			# download scripts from GitHub
+			RETRURL="${PROVISIONURL}/retrieve.sh"
+			[ ! -f "retrieve.sh" ] \
+				&& wget -A.sh "$RETRURL" \
+				&& chmod u+x retrieve.sh \
+				&& chown $NONROOT retrieve.sh
+			REPOURL="${PROVISIONURL}/repostrap.sh"
+			[ ! -f "repostrap.sh" ] \
+				&& wget -A.sh "$REPOURL" \
+				&& chmod u+x repostrap.sh \
+				&& chown $NONROOT repostrap.sh
+
+			# if remote host specified also provision from private repos through ssh
+			RLEN=$(echo "$REMOTEHOST" | wc -c)
+			if [ "$RLEN" -lt 4 ]; then
+				su $NONROOT -c "./retrieve.sh"
+				echo "run bootstrap.sh again with the -s flag to continue provisioning from private repos"
+			else
+				su $NONROOT -c "./retrieve.sh -u $USER -p $PORT -s $REMOTEHOST"
+			fi;
+
+			# cleanup provision scripts
+			[ -f "Vagrantfile" ]  && rm Vagrantfile
+			[ -f "retrieve.sh" ]  && rm retrieve.sh
+			[ -f "repostrap.sh" ] && rm repostrap.sh
 		fi;
 	) \
 	|| ~/.dotfiles/bootstrap.sh -n $NONROOT -p $PORT -u $USER -s $REMOTEHOST
+fi;
 
