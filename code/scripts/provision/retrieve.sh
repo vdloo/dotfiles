ce2457e3685b46be330b090d3a4e89f08cd1d1fd
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-22 20:04:49 +0000

    retrieve getopts from retrieve.sh

--- code/scripts/provision/retrieve.sh
+++ code/scripts/provision/retrieve.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
 # Clone public dotfiles and symlink them to ~, then
-# run repostrap. If remote host specified, also clone
-# dotfiles from that source via ssh and run repostrap
-# example ./retrieve.sh -p 1234 -u username -h example.com
-
-unset USER
-unset REMOTEHOST
-unset PORT
-PORT=22
-USER=$(logname)
-REMOTEHOST="UNDEFINED"
-
-while getopts "p:u:s:" opt; do
-	case "$opt" in
-		p)
-			PORT="$OPTARG" ;;
-		u)
-			USER="$OPTARG" ;;
-		s)
-			REMOTEHOST="$OPTARG" ;;
-	esac
-done
+# run repostrap. If repostra-private.sh exists, also clone
+# dotfiles from that source via ssh
 
 [ -f repostrap-public.sh ] \
 	&& ./repostrap-public.sh
