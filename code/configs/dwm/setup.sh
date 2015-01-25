bc11010b0223ea43c94dbe24c01e40c3d3b76c21
Rick van de Loo <rickvandeloo@gmail.com>
2015-01-25 06:28:01 -0500

    Reminder to install xinerama stuff
    To prevent:
    dwm.c:40:37: fatal error: X11/extensions/Xinerama.h: No such file or directory
     #include <X11/extensions/Xinerama.h>
                                         ^

--- code/configs/dwm/setup.sh
+++ code/configs/dwm/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
+# Remember to install xinerama deps on fresh machines
+# sudo apt-get install libx11-dev libxinerama-dev
+
 #type -p git 1> /dev/null && (
 #	 [ ! -d dwm ] && git clone http://git.suckless.org/dwm || (cd dwm; git fetch; git reset --hard origin/master)
 #)
