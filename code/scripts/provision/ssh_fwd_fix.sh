961b1cb10e61afd86515623b79252f75c04f5b93
Rick van de Loo <rickvandeloo@gmail.com>
2015-07-31 14:41:02 +1000

    bootstrap.sh is replaced with puppet playbook

--- code/scripts/provision/ssh_fwd_fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# make the ssh agent forward available to root during vagrant provisioning
-AGENTF="/etc/sudoers.d/root_ssh_agent"
-if [ ! -f $AGENTF ]; then
-	echo "Defaults env_keep += \"SSH_AUTH_SOCK\"" > $AGENTF
-	chmod 0440 $AGENTF
-fi;
