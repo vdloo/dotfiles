4c596254ca21b361d38c3ff6dde423808cfa713f
Rick van de Loo <rickvandeloo@gmail.com>
2016-06-12 23:12:28 +0200

    fix bug with missing saved_items in function

--- code/scripts/fun/combinations_with_specified_repeat_of_a_powerset.py
+++ code/scripts/fun/combinations_with_specified_repeat_of_a_powerset.py
@@ -5,17 +5,11 @@
 # the possible orders of a list including missing and repeating items.
 
 # Examples: 
-# combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 0)
+#combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 0)
 # [['A'], ['B']]
 
 # combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 1)
-# [['B'], ['A', 'B'], ['B']]
-
-# combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 2)
-# [['B'], ['A', 'B'], ['A', 'A', 'B'], ['B', 'A', 'B'], ['B'], ['B', 'B'], ['A', 'B', 'B']]
-
-# combinations_with_specified_repeat_of_a_powerset(['A', 'B', 'C'], 1)
-# [['C'], ['B', 'C'], ['A', 'B', 'C'], ['C'], ['C'], ['B', 'C'], ['C'], ['C'], ['B', 'C'], ['C']]
+# [['A'], ['B'], ['A'], ['B', 'A'], ['B'], ['A', 'B'], ['A', 'B'], ['A'], ['B'], ['B'], ['A', 'B'], ['B'], ['A', 'B']]
 
 # A powerset looks like: powerset([1, 2, 3]) -> [[1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
 # A combination looks like: combinations([1, 2]) -> [[1, 2], [2, 1]]
@@ -38,6 +32,7 @@ def combinations_with_specified_repeat_of_a_powerset(s, r, saved=list()):
         combinations = list()
         for item in s:
             new_saved = list()
+            new_saved.extend(saved)
             new_saved.append([item])
             for combination in saved:
                 if r > len(filter(lambda x: x == item, combination)):
@@ -51,6 +46,4 @@ print combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 0)
 
 print combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 1)
 
-print combinations_with_specified_repeat_of_a_powerset(['A', 'B'], 2)
-
-print combinations_with_specified_repeat_of_a_powerset(['A', 'B', 'C'], 1)
+print combinations_with_specified_repeat_of_a_powerset(['A', 'B', 'C'], 2)
